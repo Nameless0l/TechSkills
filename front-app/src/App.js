726e10c716560e42ae28d9d8f31d72f8573c9ab7
KamgaBrayan <brayanarmel@gmail.com>
2024-08-26 16:35:28 +0200

    App router established in route folder

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+// App router  
+import AppRouter from './routes/router'
+
 function App() {
   return (
     <div className="App">
       <div>
         <h1 className="text-3xl text-red-500">Hello, Front app</h1>
+        <h2 className="text-lara-green">Hi, front app</h2>
+        <h3 className='text-lara-violet'>Hello world</h3>
       </div>
     </div>
   );
---
+++ front-app/src/routes/router.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export default function AppRouter(){
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                
+            </Routes>
+        </BrowserRouter>
+    );
+}
