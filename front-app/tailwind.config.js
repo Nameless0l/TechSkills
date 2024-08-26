8e4f920b682c4e783b48188bc219e5b26c668d92
KamgaBrayan <brayanarmel@gmail.com>
2024-08-26 15:53:40 +0200

    Color definition - Tailwind css

---
+++ front-app/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "lara-home-edge": "#141823",
+        "lara-search":  "#151924",
+        "lara-button": "#4c83c6",
+        "lara-bio-box": "#232c3d",
+        "lara-comment-box": "#232c3d",
+        "lara-text-category": "#2c3648",
+        "lara-post-box": "#232c3d",
+        "lara-text-header": "#fbfbfb",
+        "lara-login-box": "#181e2a",
+        "lara-text-topic": "#8394b8",
+        "lara-sticker": "#e6f4ff",
+        "lara-violet": "#a76cca",
+        "lara-pink": "#ee4274",
+        "lara-gray": "#647b9a",
+        "lara-yellow": "#c8ad98",
+        "lara-green": "#54ccb1",
+        "lara-blue": "#3289f0",
+        "lara-text-small": "#7a8aa3",
+        "lara-footer-box": "#222b3c",
+        "med-button": "#191919",
+        "med-home": "#f7f4ed",
+      }
+    },
+  },
+  plugins: [],
+}
+
